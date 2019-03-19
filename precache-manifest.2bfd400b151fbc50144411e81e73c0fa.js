2a9f41faa311791d82475542eeaef63e63cede85
Dev <dev@dylans-MacBook-Pro.local>
2019-03-19 12:30:41 -0400

    Updates

---
+++ precache-manifest.2bfd400b151fbc50144411e81e73c0fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "847d429ab5a03a129d09",
+    "url": "/clickshare-example/static/css/main.67e7244c.chunk.css"
+  },
+  {
+    "revision": "847d429ab5a03a129d09",
+    "url": "/clickshare-example/static/js/main.98f02a8f.chunk.js"
+  },
+  {
+    "revision": "5f719e72f0586384c518",
+    "url": "/clickshare-example/static/js/runtime~main.71cbba34.js"
+  },
+  {
+    "revision": "c98dab21465dad3a2980",
+    "url": "/clickshare-example/static/js/2.8c93c281.chunk.js"
+  },
+  {
+    "revision": "05a5d02080b1664c0dce7c304772e712",
+    "url": "/clickshare-example/index.html"
+  }
+];
